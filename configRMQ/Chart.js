2710d271855bbd7977ee13d027f677d77e109e9f
gunawan-ctrl <gunawa414@gmail.com>
2022-08-11 13:13:41 +0700

    first commit

--- configRMQ/Chart.js
+++ configRMQ/Chart.js
@@ -1,12 +1,12 @@
-import { Line, mixins } from 'vue-chartjs'
+// import { Line, mixins } from 'vue-chartjs'
 
-export default {
-  extends: Line,
-  mixins: [mixins.reactiveProp],
-  props: ['options'],
-  mounted () {
-    this.renderChart(this.chartData, this.options, {
-      maintainAspectRatio: false
-    })
-  }
-}
+// export default {
+//   extends: Line,
+//   mixins: [mixins.reactiveProp],
+//   props: ['options'],
+//   mounted () {
+//     this.renderChart(this.chartData, this.options, {
+//       maintainAspectRatio: false
+//     })
+//   }
+// }
